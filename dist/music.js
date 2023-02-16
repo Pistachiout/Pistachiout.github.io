e88b760846ba3fe65e8bad8b6454fdf3e7d44dae
Pistachiout <1105358600@qq.com>
2023-02-16 19:00:17 +0800

    Site updated: 2023-02-16 18:59:57

---
+++ dist/music.js
@@ -0,0 +1,75 @@
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  // autoplay: true,
+  audio: [
+    {
+      name: '光るなら-若能绽放光芒',
+      artist: '多多poi / 鹿喑kana / kinsen / 花玲 / 喵☆酱 / 宴宁 / shourei小N / 废柴萧翟',
+      url: 'http://music.163.com/song/media/outer/url?id=1840175166.mp3',
+      cover: 'https://p1.music.126.net/dxWuRgzjubpH6wbuhtttKQ==/109951165923610661.jpg?param=130y130',
+    },
+    {
+      name: '大田後生仔',
+      artist: '王玉萌',
+      url: 'http://music.163.com/song/media/outer/url?id=1396602869.mp3',
+      cover: 'http://p1.music.126.net/hITz5arBFLYC4_3D1gu2qw==/109951164423988553.jpg?param=130y130',
+    },
+    {
+      name: '昨日青空',
+      artist: '尤长靖',
+      url: 'http://sp.sycdn.kuwo.cn/c1d1dbf5b354f164b7d91c6e19113460/63edeeb2/resource/n1/23/52/4172959540.mp3',
+      cover: 'https://p2.music.126.net/txikZRkBTTEidW-sHyz_jA==/109951163575106477.jpg?param=130y130',
+    },
+    {
+      name: '再见，昨天',
+      artist: '牛奶咖啡',
+      url: 'http://lo.sycdn.kuwo.cn/7b9e3e5501a3f1408fbfe90f8e6ac32f/63eded84/resource/n2/4/23/1784629011.mp3',
+      cover: 'https://p1.music.126.net/Fkuj5XGCBf4_SL_T_LkDCg==/109951164776987867.jpg?param=130y130',
+    },
+    {
+      name: '明天，你好',
+      artist: '牛奶咖啡',
+      url: 'http://ip.h5.rh01.sycdn.kuwo.cn/3ac642c32d7f0e649ee4aea245ab8e9b/63ededbe/resource/n3/42/87/2260559662.mp3',
+      cover: 'https://y.qq.com/music/photo_new/T002R300x300M000003K4mFV3B9UfM_1.jpg?max_age=2592000',
+    },
+    {
+      name: '越长大越孤单',
+      artist: '牛奶咖啡',
+      url: 'http://ip.h5.ra01.sycdn.kuwo.cn/ccc95cb12e736c966f5f1ed2313c9d94/63edee13/resource/n3/320/89/97/762329787.mp3',
+      cover: 'https://pic.baike.soso.com/ugc/baikepic2/0/20220728105324-1333779359_jpg_462_454_24779.jpg/800',
+    },
+    {
+      name: '爸爸妈妈',
+      artist: '李荣浩',
+      url: 'http://music.163.com/song/media/outer/url?id=407450223.mp3',
+      cover: 'https://p2.music.126.net/e-Uc6W3Kug-HFHJ5nvCUPg==/109951166562828988.jpg?param=130y130',
+    },
+    {
+      name: '像我这样的人',
+      artist: '毛不易',
+      url: 'http://music.163.com/song/media/outer/url?id=569213220.mp3',
+      cover: 'https://p2.music.126.net/vmCcDvD1H04e9gm97xsCqg==/109951163350929740.jpg?param=130y130',
+    },
+    {
+      name: '可能否',
+      artist: '木小雅',
+      url: 'http://music.163.com/song/media/outer/url?id=569214126.mp3',
+      cover: 'https://p1.music.126.net/SJYnDay7wgewU3O7tPfmOQ==/109951163322541581.jpg?param=130y130',
+    },
+    {
+      name: '深海少女',
+      artist: '初音ミク',
+      url: 'http://music.163.com/song/media/outer/url?id=31140699.mp3',
+      cover: 'https://p2.music.126.net/8jr6ElaxvntFk8LkKM4j2A==/7880199836870994.jpg?param=130y130',
+    },
+    {
+      name: '世间美好与你环环相扣',
+      artist: '柏松',
+      url: 'http://music.163.com/song/media/outer/url?id=1363948882.mp3',
+      cover: 'https://p2.music.126.net/DK1_4sP_339o5rowMdPXdw==/109951164071024476.jpg?param=130y130',
+    },
+    
+  ]
+});
+
